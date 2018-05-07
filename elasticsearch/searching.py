6536df1eb87c0051faf26dc398e0e4f2ba3e25a5
sabel94 <38723433+sabel94@users.noreply.github.com>
2018-05-08 00:59:16 +0200

    added query matching and filters

--- elasticsearch/searching.py
+++ elasticsearch/searching.py
@@ -3,128 +3,91 @@ from elasticsearch import Elasticsearch
 
 #The user should be able to provide these values as input through the user interface.
 #-----------------------
-query = "Honda TRX700 vägreggad honda"
+query = "Husqvarna 250"
 city = "Eskilstuna"
-vehicle_type = "Sport"
-min_price = 17000
-max_price = 20000
-min_model_year = 2017
+vehicle_type = "Enduro"
+min_price = 10000
+max_price = 50000
+min_model_year = 2014
 max_model_year = 2017
 #-----------------------
 max_number_of_search_results = 5;
 es = Elasticsearch()
 es.indices.refresh(index="simple")
 
+
 if city == "All" and vehicle_type == "All":
     elastic_query = {
         "query":{
                 "bool":{
+                    "must":[
+                        {"range": {"price": {"gte": min_price, "lte": max_price}}},
+                        {"range": {"modelYear": {"gte": min_model_year, "lte": max_model_year}}}
+                    ],
                     "should":[
                         {"match": {"title":  query}},
-                        {"match": {"description": query}}],
-                    "filter":{
-                        "range":{
-                            "price":{
-                                "gte": min_price,
-                                "lte": max_price
-                            }
-                        },
-                        "range":{
-                            "modelYear":{
-                                "gte": min_model_year,
-                                "lte": max_model_year
-                            }
-                        }
-                    }
+                        {"match": {"description": query}}
+                    ]
                 }
         },
         "size": max_number_of_search_results
     }
-
+    
 elif vehicle_type == "All":
     elastic_query = {
         "query":{
                 "bool":{
-                    "must":
+                    "must":[
                         {"match": {"location": city}},
+                        {"range": {"price": {"gte": min_price, "lte": max_price}}},
+                        {"range": {"modelYear": {"gte": min_model_year, "lte": max_model_year}}}
+                    ],
                     "should":[
                         {"match": {"title":  query}},
-                        {"match": {"description": query}}],
-                    "filter":{
-                        "range":{
-                            "price":{
-                                "gte": min_price,
-                                "lte": max_price
-                            }
-                        },
-                        "range":{
-                            "modelYear":{
-                                "gte": min_model_year,
-                                "lte": max_model_year
-                            }
-                        }
-                    }
+                        {"match": {"description": query}}
+                    ]
                 }
         },
         "size": max_number_of_search_results
     }
-        
+    
 elif city == "All":
     elastic_query = {
         "query":{
                 "bool":{
-                    "must":
+                    "must":[
                         {"match": {"vehicleType": vehicle_type}},
+                        {"range": {"price": {"gte": min_price, "lte": max_price}}},
+                        {"range": {"modelYear": {"gte": min_model_year, "lte": max_model_year}}}
+                    ],
                     "should":[
                         {"match": {"title":  query}},
-                        {"match": {"description": query}}],
-                    "filter":{
-                        "range":{
-                            "price":{
-                                "gte": min_price,
-                                "lte": max_price
-                            }
-                        },
-                        "range":{
-                            "modelYear":{
-                                "gte": min_model_year,
-                                "lte": max_model_year
-                            }
-                        }
-                    }
+                        {"match": {"description": query}}
+                    ]
                 }
         },
         "size": max_number_of_search_results
     }
-            
+    
 else:
     elastic_query = {
         "query":{
                 "bool":{
                     "must":[
                         {"match": {"location": city}},
-                        {"match": {"vehicleType": vehicle_type}}],
+                        {"match": {"vehicleType": vehicle_type}},
+                        {"range": {"price": {"gte": min_price, "lte": max_price}}},
+                        {"range": {"modelYear": {"gte": min_model_year, "lte": max_model_year}}}
+                    ],
                     "should":[
                         {"match": {"title":  query}},
-                        {"match": {"description": query}}],
-                    "filter":{
-                        "range":{
-                            "price":{
-                                "gte": min_price,
-                                "lte": max_price
-                            }
-                        },
-                        "range":{
-                            "modelYear":{
-                                "gte": min_model_year,
-                                "lte": max_model_year
-                            }
-                        }
-                    }
+                        {"match": {"description": query}}
+                    ]
                 }
         },
         "size": max_number_of_search_results
     }
+
 
 #execute query
 res = es.search(index="simple", body=elastic_query)
