1ce6cd261f2a39f45dd4c497dbf3aafa38f2d397
Pierre Cournut <pierre.cournut@gmail.com>
2018-05-16 11:03:31 +0200

    Add files via upload

--- elasticsearch/SearchGUI.py
+++ elasticsearch/SearchGUI.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from searcher import *
+import PIL
+from PIL import ImageTk, Image
 
 default_values = ('Honda', 'All')
 fields = 'Query', 'City'
@@ -56,15 +58,18 @@ def search(entries):
 	"""
 		Use searcher.py to create and display results
 	"""
+	# Handles VehicleType = All
 	if entries[2][1].get() == "All":
 		res = searcher.price(query=entries[0][1].get(), location=entries[1][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
 		returned_docs = searcher.similar(query=entries[0][1].get(), location=entries[1][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
-		if entries[1][1].get() == "All":
+		# Handles Location = All
+		if entries[1][1].get() == "All" or entries[1][1].get() == "":
 			res = searcher.price(query=entries[0][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
 			returned_docs = searcher.similar(query=entries[0][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
-	elif entries[1][1].get() == "All":
-		res = searcher.price(query=entries[0][1].get(), location=entries[2][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
-		returned_docs = searcher.similar(query=entries[0][1].get(), location=entries[2][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
+	# Handles Location = All 
+	elif entries[1][1].get() == "All" or entries[1][1].get() == "":
+		res = searcher.price(query=entries[0][1].get(), vehicle_type=entries[2][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
+		returned_docs = searcher.similar(query=entries[0][1].get(), vehicle_type=entries[2][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
 	else:
 		res = searcher.price(query=entries[0][1].get(), location=entries[1][1].get(), vehicle_type=entries[2][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
 		returned_docs = searcher.similar(query=entries[0][1].get(), location=entries[1][1].get(), vehicle_type=entries[2][1].get(),  min_model_year=entries[3][1].get(), max_model_year=entries[4][1].get())
@@ -80,21 +85,21 @@ def search(entries):
 			field = entry[0]
 			text = entry[1].get()
 			Label(res_frame, text='%s: "%s"' % (field, text)).pack(side=TOP)
-		Label(res_frame, text='Number of hits: %s' % len(returned_docs)).pack(side=TOP,padx=20,pady=20)
+		Label(res_frame, text='Number of hits: %s' % len(returned_docs), bg='grey', fg='white').pack(side=TOP,padx=20,pady=20)
 		w = Label(res_frame, text="""
 	    Average price: {}
 	    Median price: {}
 	    Max price: {}
 	    Min price: {}
 	    """.format(\
-	        res["average_price"],
+	        round(res["average_price"],2),
 	        res["median_price"],
 	        res["max_price"],
 	        res["min_price"],
 	        ))
 		w.pack(side=LEFT, padx=20, pady=20)
 
-	b = Button(res_frame, text='Quit', command=res_frame.quit)
+	b = Button(res_frame, text='Quit', command=res_frame.destroy)
 	b.pack(side=BOTTOM, padx=5, pady=5)
 
 	res_frame.mainloop()
@@ -102,16 +107,21 @@ def search(entries):
 if __name__ == '__main__':
 
 	root = Tk()
+
+	img = ImageTk.PhotoImage(Image.open('ducati_draw.jpg').resize((500,250)))
+	panel = Label(root, image = img)
+	panel.pack(side = "top", fill = "both", expand = "yes")
+
 	root.title('Searcher')
 	searcher = Searcher()
 
 	ents = makeform(root, fields)
 	root.bind('<Return>', (lambda event, e=ents: search(e)))   
 
-   # Quit button
-	b1 = Button(root, text='Quit', command=root.quit)
+	# Quit button
+	b1 = Button(root, text='Quit', command=quit)
 	b1.pack(side=LEFT, padx=5, pady=5)
-   # Search button
+	# Search button
 	b2 = Button(root, text='Search', command=(lambda e=ents: search(e)))
 	b2.pack(side=RIGHT, padx=5, pady=5)
 
